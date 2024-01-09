51052cb91c0f7fdfdc0b6c785dc5ee9a245990c6
[shubhamsingh74] <shubhamsingh74970@gmail.com>
2024-01-09 23:43:09 +0530

    till classical bloc

---
+++ lib/features/home/home_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Posts')),
+    );
+  }
+}
